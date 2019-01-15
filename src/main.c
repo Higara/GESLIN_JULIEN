f333822ae1833aea75e4b9dce7d43149f8b839db
Thomas Geslin <thomasgeslin91@hotmail.fr>
2019-01-15 14:20:23 +0100

    initialisation du projet

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "image.h"
+
+
+int main(int argc, char **argv)
+{
+    // create an image
+    Image image;
+
+    // load a ppm file
+    if(loadImagePPM(&image,"../images/pogchamp.ppm") != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+
+    // modify the image (red pixel in the center)
+    unsigned int pixel = (image.width*(image.height/2) + (image.width/2))*3;
+    printf("%d\n",pixel);
+    image.data[pixel + 0] = 255; // Red
+    image.data[pixel + 1] = 0; // Green
+    image.data[pixel + 2] = 0; // Blue
+
+    // save the image (if the directory "images" already exists)
+    saveImagePPM(&image, "../images/output.ppm");
+
+    // free the image memory
+    freeImage(&image);
+
+    return 0;
+}
+
+
